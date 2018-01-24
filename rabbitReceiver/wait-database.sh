ec10e819b2d3177a630f06edcfcff56247898134
pavel.sinitski <pavel.sinitski@bpmobile.com>
2018-01-24 17:57:05 +0300

    Added monitoring service.

--- rabbitReceiver/wait-database.sh
+++ rabbitReceiver/wait-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Waiting for mysql";
-while ! mysqladmin ping -h"$MYSQL_HOST" -P"$MYSQL_PORT";
+while ! mysqladmin ping -h"$MYSQL_HOST" -P"$MYSQL_PORT" --silent;
 do
     printf ".";
     sleep 3;
--- rabbitSender/wait-rabbit.sh
+++ rabbitSender/wait-rabbit.sh
@@ -2,7 +2,6 @@
 echo Waiting for rabbitmq service start...;
 while ! nc -z "$RABBIT_HOST" "$RABBIT_PORT";
 do
-  printf "."
   sleep 3;
 done;
 echo Connected!;
